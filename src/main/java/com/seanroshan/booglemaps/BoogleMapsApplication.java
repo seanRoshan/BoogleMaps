20c7c42df954e23eda9c8a3ae9d6eb0efe3107ca
SEAN ROSHAN <sroshan@dentalxchange.com>
2020-07-03 19:39:51 -0700

    feat: The mock service returns random addresses

---
+++ src/main/java/com/seanroshan/booglemaps/BoogleMapsApplication.java
@@ -0,0 +1,13 @@
+package com.seanroshan.booglemaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoogleMapsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoogleMapsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/seanroshan/booglemaps/controller/MapsController.java
@@ -0,0 +1,20 @@
+package com.seanroshan.booglemaps.controller;
+
+import com.seanroshan.booglemaps.entity.Address;
+import com.seanroshan.booglemaps.repository.MockAddressRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/maps")
+public class MapsController {
+
+    @GetMapping
+    public Address get(@RequestParam Double lat,
+                       @RequestParam Double lon) {
+        return MockAddressRepository.getRandom();
+    }
+
+}
